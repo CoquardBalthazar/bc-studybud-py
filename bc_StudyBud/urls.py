195f0a775fcfa2e70a5cabfd8f9f6a548d225660
BC Ubuntu <balthazar.coquard@gmail.com>
2024-02-20 06:48:49 +0100

    Index modified

--- bc_StudyBud/urls.py
+++ bc_StudyBud/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Home page')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))
+    path('', home, NAME='home')
 ]
+
